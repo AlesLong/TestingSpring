8dec8006a288a8b6c04e8bfd574d4a83edc5ddc1
AlesLong <yevbes@gmail.com>
2021-01-30 01:18:06 +0200

    Init-Destroy-Factory Method

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,22 +7,25 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml"
         );
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        
+        System.out.println(classicalMusic.getSong());
 
-        //Music music = context.getBean("musicBean", Music.class);
+        // Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//            MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//            MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-        System.out.println(comparison);
+//            boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//            System.out.println(comparison);
 
-        firstMusicPlayer.setVolume(10);
+//            firstMusicPlayer.setVolume(10);
 
-        System.out.println(firstMusicPlayer.getName());
-        System.out.println(firstMusicPlayer.getVolume());
+//            System.out.println(firstMusicPlayer.getName());
+//            System.out.println(firstMusicPlayer.getVolume());
 
-        System.out.println(secondMusicPlayer.getName());
-        System.out.println(secondMusicPlayer.getVolume());
+//            System.out.println(secondMusicPlayer.getName());
+//            System.out.println(secondMusicPlayer.getVolume());
 
         context.close();
     }
