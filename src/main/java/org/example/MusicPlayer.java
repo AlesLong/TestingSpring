df4ff0119615e94d2cfa74b2ddd6532af1991a99
AlesLong <yevbes@gmail.com>
2021-01-31 18:54:44 +0200

    @Autowired Constructor(2beans)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -8,16 +8,19 @@ import java.util.List;
 @Component
 public class MusicPlayer {
     private ClassicalMusic classicalMusic;
-//    private String name;
-//    private int volume;
+    private RockMusic rockMusic;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic) {
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
         this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
+
 
     public void playMusic() {
         System.out.println("Playing : " + classicalMusic.getSong());
+        System.out.println("Playing : " + rockMusic.getSong());
+
     }
 
 //    public Music getMusic() {
