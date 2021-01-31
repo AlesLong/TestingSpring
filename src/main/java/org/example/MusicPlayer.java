049d687a3541bcca171559d45fbd7c27f12a05d8
AlesLong <yevbes@gmail.com>
2021-01-31 19:01:21 +0200

    @Autowired Constructor(2beans)

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -3,8 +3,6 @@ package org.example;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class MusicPlayer {
     private ClassicalMusic classicalMusic;
@@ -17,35 +15,7 @@ public class MusicPlayer {
     }
 
 
-    public void playMusic() {
-        System.out.println("Playing : " + classicalMusic.getSong());
-        System.out.println("Playing : " + rockMusic.getSong());
-
+    public String playMusic() {
+        return "Playing : " + classicalMusic.getSong();
     }
-
-//    public Music getMusic() {
-//        return music;
-//    }
-//
-//    public void setMusic(Music music) {
-//        this.classicalMusic = music;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getVolume() {
-//        return volume;
-//    }
-//
-//    public void setVolume(int volume) {
-//        this.volume = volume;
-//    }
-
-
 }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -14,8 +14,11 @@ public class TestSpring {
 //        MusicPlayer musicPlayerClassical = new MusicPlayer(music2);
 //        musicPlayer.playMusic();
 //        musicPlayerClassical.playMusic();
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+       // MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        //musicPlayer.playMusic();
+
+        Computer computer = context.getBean("computer",Computer.class);
+        System.out.println(computer);
         context.close();
     }
 }
